aeddba7c50e821483db2be212a7798278b6b6426
averymj <averyjohntx@gmail.com>
2026-03-27 20:24:24 +0000

    Deploying to gh-pages from @ averymj/averymj.github.io@5252d2d0f6ae93bf29ee54ba8f9648242bbe9148 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-projects",
           title: "projects",
-          description: "A growing collection of your cool projects.",
+          description: "Check out the projects I&#39;ve done!",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
